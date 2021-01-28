611a8978651d2112baa2293968cdb2acb57c3dd3
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-01-29 01:57:44 +0600

    Setup initial application.

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const morgan = require('morgan');
+// const winston = require('winston');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const passport = require('passport');
+require('./config/passport')(passport);
+
+// Setting up Knex
+const { Model } = require('objection');
+const Knex = require('knex');
+const apiRoutesV1 = require('./api/index');
+const knexSettings = require('./knexfile');
+
+const knex = Knex(knexSettings.development);
+Model.knex(knex);
+
+// Initializing our app 🎉
+const app = express();
+
+// Some Security Headers. 👀
+app.use(helmet());
+
+// Enabling CORS for all routes. 🛩
+app.use(cors());
+
+// Parsing the body. 💪
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parsing the Cookies. 🍪
+app.use(cookieParser());
+
+// Passport for Authentication. 🛂
+app.use(passport.initialize());
+
+// At last 😌, We are trying the match any routes.
+app.use('/api/v1/', apiRoutesV1);
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running ➡️  PORT ${server.address().port}`);
+});
